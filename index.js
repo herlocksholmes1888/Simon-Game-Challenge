fa0c26d0bbf317b110e66e37b52c52a8004ee774
Luisa Frugoli <luisafrugoli11@gmail.com>
2024-03-04 13:57:09 -0300

    :sparkles: add: basic pattern creation

---
+++ index.js
@@ -0,0 +1,8 @@
+let currentPattern = [];
+
+let buttonColours = ["red", "blue", "green", "yellow"];
+let randomColour = Math.floor(Math.random() * buttonColours.length);
+
+currentPattern.push(randomColour);
+
+console.log(currentPattern);
