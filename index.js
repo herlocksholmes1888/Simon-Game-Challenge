70005852acde874c9e4a29010ee94dfa7fdf95ab
Luisa Frugoli <luisafrugoli11@gmail.com>
2024-03-12 15:22:45 -0300

    :sparkles: add fade out animation

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ let currentPattern = [];
 
 let buttonColours = ["red", "blue", "green", "yellow"];
 let randomColour = Math.floor(Math.random() * buttonColours.length);
-
 currentPattern.push(randomColour);
 
-console.log(currentPattern);
+$("#" + buttonColours[randomColour]).fadeOut(100).fadeIn(100);
