0209058eead3c4f88080f8d82be973c552cba7d1
Luisa Frugoli <luisafrugoli11@gmail.com>
2024-03-13 23:45:43 -0300

    :sparkles: add user pattern memory

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ let buttonColours = ["red", "blue", "green", "yellow"];
 let userClickedPattern = [];
 
 $(document).keypress(function(){
-    let randomColourIndex = Math.floor(Math.random() * buttonColours.length);
-    let randomColour = buttonColours[randomColourIndex];
-    currentPattern.push(randomColour);
+    //function nextSequence() {
+        let randomColourIndex = Math.floor(Math.random() * buttonColours.length);
+        let randomColour = buttonColours[randomColourIndex];
+        currentPattern.push(randomColour);
 
-    $("#" + randomColour).fadeOut(100).fadeIn(100);
+        $("#" + randomColour).fadeOut(100).fadeIn(100);
 
-    new Audio("sounds/" + randomColour + ".mp3").play();
+        new Audio("sounds/" + randomColour + ".mp3").play();
+    //}
+
+    $(".btn").on("click", function(){
+        userClickedPattern.push("#" + $(this).attr("id"));
+        console.log(userClickedPattern);
+    });
 });
