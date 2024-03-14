5ba36bc86dce0cad9b6a1fa930a80209cc05ab54
Luisa Frugoli <luisafrugoli11@gmail.com>
2024-03-14 15:28:58 -0300

    :sparkles: add sound user click

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ let buttonColours = ["red", "blue", "green", "yellow"];
 let userClickedPattern = [];
 
 $(document).keypress(function(){
-    //function nextSequence() {
+    function nextSequence() {
         let randomColourIndex = Math.floor(Math.random() * buttonColours.length);
         let randomColour = buttonColours[randomColourIndex];
         currentPattern.push(randomColour);
 
         $("#" + randomColour).fadeOut(100).fadeIn(100);
 
-        new Audio("sounds/" + randomColour + ".mp3").play();
-    //}
+        playSound(randomColour);
+    }
 
-    $(".btn").on("click", function(){
-        userClickedPattern.push("#" + $(this).attr("id"));
-        console.log(userClickedPattern);
+    function playSound(button) {
+        new Audio("sounds/" + button + ".mp3").play();
+    }
+
+    $(".btn").on("click", function(){;
+        let userChosenColour = $(this).attr("id");
+        userClickedPattern.push("#" + userChosenColour);
+
+        playSound(userChosenColour);
     });
+
+    nextSequence();
 });
