780a44ef536fd59317e912f83f9e1cf2119daf8b
Luisa Frugoli <luisafrugoli11@gmail.com>
2024-03-18 17:43:15 -0300

    :sparkles: add pattern comparison

--- index.js
+++ index.js
@@ -4,47 +4,72 @@ let userClickedPattern = [];
 let hasStarted = false;
 let level = 0;
 
-function nextSequence() {
-    level += 1;
-    $("#level-title").html("Level " + level);
-
-    let randomColourIndex = Math.floor(Math.random() * buttonColours.length);
-    let randomColour = buttonColours[randomColourIndex];
-    currentPattern.push(randomColour);
-
-    buttonAnimation(randomColour)
-    playSound(randomColour);
-}
-
-function userInteraction() {
-    $(".btn").on("click", function(){
-        let userChosenColour = $(this).attr("id");
-        userClickedPattern.push(userChosenColour);
-
-        buttonAnimation(userChosenColour);
-        playSound(userChosenColour);
-    });
-}
-
-function playSound(button) {
-    new Audio("sounds/" + button + ".mp3").play();
-}
-
-function buttonAnimation(button) {
-    let clickedButton = $("#" + button);
-    clickedButton.addClass("pressed");
-    clickedButton.fadeOut(100).fadeIn(100);
-
-    setTimeout(function(){
-        clickedButton.removeClass("pressed");
-    }, 150);
-}
-
 $(document).keypress(function(){
+    function nextSequence() {
+        level += 1;
+        $("#level-title").html("Level " + level);
+
+        // Reset userClickedPattern each new sequence
+        userClickedPattern = [];
+    
+        let randomColourIndex = Math.floor(Math.random() * buttonColours.length);
+        let randomColour = buttonColours[randomColourIndex];
+        currentPattern.push(randomColour);
+
+        console.log(currentPattern);
+    
+        buttonAnimation(randomColour)
+        playSound(randomColour);
+    }
+    
+    function userInteraction() {
+        $(".btn").on("click", function(){
+            let userChosenColour = $(this).attr("id");
+            userClickedPattern.push(userChosenColour);
+
+            console.log(userClickedPattern);
+    
+            buttonAnimation(userChosenColour);
+            playSound(userChosenColour);
+
+            checkAnswer();
+        });
+    }
+
+    function checkAnswer() {
+        if (userClickedPattern.length === currentPattern.length){
+            if (JSON.stringify(currentPattern) === JSON.stringify(userClickedPattern)) {
+                console.log("Success!");
+    
+                if (userClickedPattern.length === currentPattern.length) {
+                    setTimeout(function(){
+                        nextSequence();
+                    }, 1500);
+                }
+            }
+            else {
+                console.log("Failure!");
+            }
+        }
+    }
+
+    function playSound(button) {
+        new Audio("sounds/" + button + ".mp3").play();
+    }
+    
+    function buttonAnimation(button) {
+        let clickedButton = $("#" + button);
+        clickedButton.addClass("pressed");
+        clickedButton.fadeOut(100).fadeIn(100);
+    
+        setTimeout(function(){
+            clickedButton.removeClass("pressed");
+        }, 150);
+    }
+
     if (hasStarted === false) {
         nextSequence();
         hasStarted = true;
     }
-
     userInteraction();
 });
